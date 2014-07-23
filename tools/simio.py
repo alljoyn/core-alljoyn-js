7c322b5816973798dda28d0f3af26b8828b1704b
Mathew Martineau <mathewm@qce.qualcomm.com>
2014-07-23 16:09:18 -0700

    Add Windows support to simio.py
    Change-Id: Ia282e7382d4005cf8022c5f9cbebbab2baf27d03
    Signed-off-by: Mathew Martineau <mathewm@qce.qualcomm.com>

--- tools/simio.py
+++ tools/simio.py
@@ -18,6 +18,21 @@
 import Tkinter as Tk
 import socket
 import os
+import sys
+
+try:
+    import win32pipe
+    import win32file
+    import win32api
+    import win32event
+    import pywintypes
+    import winerror
+    import threading
+    import Queue
+except ImportError:
+    if sys.platform == 'win32':
+        print "Could not find win32 modules"
+        sys.exit(-1)
 
 class Pin(object):
     """Pin abstraction - common functionality across input and output pins"""
@@ -102,16 +117,30 @@ class InputButton(Pin, Tk.Button):
             #print "Rising"
             self.server.send_interrupt(self, Pin.trigger_rising)
 
+def do_pipe_connect(handle, queue):
+    """Wait for an incoming connection and report status"""
+    try:
+        status = win32pipe.ConnectNamedPipe(handle, None)
+    except:
+        status = -1
+
+    if status == 0:
+        queue.put("connected", False)
+    else:
+        queue.put("disconnected", False)
+
 class Simio(object):
     """
     Overall application object.
-    
+
     Handles socket IO and GUI organization.
     """
 
     server_name = "/tmp/ajs_gui"  # Path to socket
     command_len = 3               # Bytes per incoming command
     columns = 4                   # Number of columns in pin display
+    pipe_poll_ms = 50             # Milliseconds between pipe reads on Windows
+    pipe_quick_poll_ms = 1        # Quick interval for consecutive incoming commands
 
     def __init__(self, master):
         self.master = master
@@ -121,9 +150,15 @@ class Simio(object):
 
         self.server = None
         self.sock = None
+        self.pipe = None
+        self.pipe_status = "disconnected"
+        self.pipe_connect_thread = None
         self.readbuf = ''
 
-        self.server_setup()
+        if sys.platform == 'win32':
+            self.pipe_setup()
+        else:
+            self.server_setup()
 
         f = frame
         self.pins = [Led(f, color="red"),
@@ -145,11 +180,52 @@ class Simio(object):
         self.button = Tk.Button(frame, text="Quit", command=frame.quit)
         self.button.grid(columnspan=self.columns, padx=20, pady=20)
 
+    def pipe_connect(self):
+        if self.pipe_status == "connected":
+            try:
+                win32pipe.DisconnectNamedPipe(self.pipe)
+            except:
+                pass
+            self.pipe_status = "disconnected"
+
+        if self.pipe_status == "disconnected":
+            self.pipe_status = "waiting"
+            self.pipe_connect_thread = threading.Thread(target=do_pipe_connect,
+                                                        args=(self.pipe,self.connectq))
+            self.pipe_connect_thread.daemon = True
+            self.pipe_connect_thread.start()
+
+    def pipe_setup(self):
+        #print "Create pipe: %s" % name
+        self.connectq = Queue.Queue()
+        self.poll_id = None
+
+        pipename = ("//./pipe" + self.server_name).replace('/', '\\')
+        maxinstances = 1
+        bufsize = 2048
+        timeout = 300
+        security = None
+        self.pipe = win32pipe.CreateNamedPipe(pipename,
+                                              win32pipe.PIPE_ACCESS_DUPLEX,
+                                              win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT,
+                                              maxinstances, bufsize, bufsize, timeout, security)
+        #print "Pipe created: %s" % self.pipe
+
+        self.pipe_connect()
+        # Poll for incoming data
+        self.pipe_schedule(self.pipe_poll_ms)
+
+    def pipe_schedule(self, ms):
+        if self.poll_id is not None:
+            self.master.after_cancel(self.poll_id)
+
+        self.poll_id = self.master.after(ms, self.pipe_cb)
+
     def server_setup(self):
         """Set up incoming socket and listen for connections"""
         self.server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.server.setblocking(0)
-        
+
         try:
             os.remove(self.server_name)
         except OSError:
@@ -157,7 +233,7 @@ class Simio(object):
 
         self.server.bind(self.server_name)
         self.server.listen(1)
-        
+
         self.master.createfilehandler(self.server, Tk.tkinter.READABLE, self.server_cb)
 
     def server_shutdown(self):
@@ -173,7 +249,7 @@ class Simio(object):
         self.sock.setblocking(0)
 
         print "Accepted"
-        
+
         self.master.createfilehandler(self.sock, Tk.tkinter.READABLE, self.sock_cb)
 
     def sock_recv(self):
@@ -208,13 +284,20 @@ class Simio(object):
 
     def send_command(self, command, pin, val):
         """Pack and send a command to the client"""
+        data = command + chr(pin) + chr(val)
+        print "Send: %s" % repr(data)
         if self.sock:
-            data = command + chr(pin) + chr(val)
-            #print "sock send: %s" % repr(data)
             self.sock.send(data)
+        elif self.pipe and self.pipe_status == "connected":
+            try:
+                win32file.WriteFile(self.pipe, data, None)
+            except:
+                print "Write failed, reconnecting"
+                self.pipe_connect()
 
     def process_command(self, command):
         """Parse and perform a single command"""
+        print "Command: %s" % repr(command)
         op = command[0]
         pin = ord(command[1])
         val = ord(command[2])
@@ -235,6 +318,46 @@ class Simio(object):
             # Interrupt trigger
             self.pins[pin].setTrigger(val)
 
+    def pipe_cb(self, *args):
+        """Callback polling for incoming pipe connection and data"""
+        self.poll_id = None
+        nextpoll = self.pipe_poll_ms
+        if self.pipe_status == "waiting":
+            try:
+                self.pipe_status = self.connectq.get(False)
+            except Queue.Empty:
+                pass
+
+        if self.pipe_status != "connected":
+            self.pipe_schedule(nextpoll)
+            return
+
+        size = None
+        while size != 0:
+            try:
+                data, size, result = win32pipe.PeekNamedPipe(self.pipe, 1024)
+            except:
+                print "Pipe error, reconnecting"
+                self.pipe_connect()
+                break
+
+            if size > 0:
+                try:
+                    hr, data = win32file.ReadFile(self.pipe, size, None)
+                    self.readbuf += data
+                    nextpoll = self.pipe_quick_poll_ms
+                except:
+                    print "Read failed, reconnecting"
+                    self.pipe_connect()
+                    size = 0
+
+        while len(self.readbuf) >= self.command_len:
+            self.process_command(self.readbuf[:self.command_len])
+            self.readbuf = self.readbuf[self.command_len:]
+
+
+        self.pipe_schedule(nextpoll)
+
     def server_cb(self, fno, mask):
         """Callback for when the server socket becomes readable (incoming connection)"""
         if mask & Tk.tkinter.READABLE:
@@ -250,4 +373,7 @@ root = Tk.Tk()
 simio = Simio(root)
 
 root.mainloop()
-root.destroy()
+try:
+    root.destroy()
+except:
+    pass

6715a9e2ebc28154ef9f4e513f82cadb9274a2e7
James Prestwood <jprestwo@qce.qualcomm.com>
2015-02-11 14:12:15 -0800

    Sample app demonstrating offboarding/factory reset
    Change-Id: I1b40ff906d5355796b34f41f0e890f24fe69e0b8
    Signed-off-by: James Prestwood <jprestwo@qce.qualcomm.com>

---
+++ js/factory_reset.js
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * Copyright (c) 2015, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+/*
+ * This demonstrates AllJoyn.js' capability to reset your
+ * devices access point information (SSID, Passphrase) using
+ * offboarding. The blinking LED is your devices function
+ * and the reset push button is a "network reset" button.
+ *
+ * Say you get this "blinking" device in your home and you
+ * have onboarded it to your home router and it is blinking
+ * away as it should. You then change your routers password
+ * or SSID and you need to reconfigure the "blinking" device
+ * to the new credentials. Since the device is head-less you
+ * must have a way to reset these credentials. To do this
+ * you call AJ.offboard() which will clear any old SSID
+ * credentials and cause the device to go back into Soft AP
+ * mode and wait to be onboarded again. Linking this
+ * functionality to a button is the most realistic use case
+ * since most devices like this always have a reset button.
+ */
+
+var AJ = require('AllJoyn');
+var IO = require('IO');
+
+var reset_pb = IO.digitalIn(IO.pin[22], IO.pullDown);
+var blink = IO.digitalOut(IO.pin[7]);
+
+/* This devices only function, blink */
+function blink() {
+    blink.toggle();
+}
+
+/* Function to reset the device */
+function reset() {
+    AJ.offboard();
+}
+
+reset_pb.trigger(IO.FallingEdge, reset);
+
+setInterval(blink, 1000);

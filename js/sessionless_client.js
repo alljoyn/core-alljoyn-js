3dd5419d8b6d2ebdb596684ed88cfb80c523f3e6
James Prestwood <jprestwo@qce.qualcomm.com>
2015-04-21 07:40:17 -0700

    Added sessionless client/service to test sessionless signals
    Change-Id: I383c326f219d35a7fa8cbae9a31e8b7e314217e5
    Signed-off-by: James Prestwood <jprestwo@qce.qualcomm.com>

---
+++ js/sessionless_client.js
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+var AJ = require('AllJoyn');
+
+AJ.interfaceDefinition["org.alljoyn.sessionless"] =
+{
+    my_signal:{ type:AJ.SIGNAL, args:[{inStr:"s"}], returns:[{outStr:"s"}] }
+};
+
+AJ.objectDefinition['/org/alljoyn/sessionless'] = {
+    interfaces:['org.alljoyn.sessionless']
+};
+
+function sendSignal()
+{
+    var sig = AJ.signal('/org/alljoyn/sessionless', {my_signal:'org.alljoyn.sessionless'});
+    sig.sessionless = true;
+    sig.timeToLive = 100;
+    sig.send("signal");
+}
+
+var interval;
+
+AJ.onAttach = function()
+{
+    interval = setInterval(sendSignal, 100);
+}
+
+AJ.onDetach = function()
+{
+    clearInterval(interval);
+    print("AJ.onDetach");
+}
+
+print("SLS client test program initialized\n");

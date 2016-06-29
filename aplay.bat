4c3260b4a64fae9d609e9ab168f4dc06e76a4de9
Jorge Martinez Hernandez <jmartinez@affinegy.com>
2016-06-29 12:26:26 -0500

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I6330bf511523e3cdd70fd6b7348f052c38609f21
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- aplay.bat
+++ aplay.bat
@@ -1,16 +1,29 @@
 @rem ******************************************************************************
-@rem * Copyright AllSeen Alliance. All rights reserved.
+@rem * @rem * Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+@rem *    Source Project (AJOSP) Contributors and others.
 @rem *
-@rem *    Permission to use, copy, modify, and/or distribute this software for any
-@rem *    purpose with or without fee is hereby granted, provided that the above
-@rem *    copyright notice and this permission notice appear in all copies.
+@rem *    SPDX-License-Identifier: Apache-2.0
 @rem *
-@rem *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-@rem *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-@rem *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-@rem *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-@rem *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-@rem *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-@rem *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+@rem *    All rights reserved. This program and the accompanying materials are
+@rem *    made available under the terms of the Apache License, Version 2.0
+@rem *    which accompanies this distribution, and is available at
+@rem *    http://www.apache.org/licenses/LICENSE-2.0
+@rem *
+@rem *    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+@rem *    Alliance. All rights reserved.
+@rem *
+@rem *    Permission to use, copy, modify, and/or distribute this software for
+@rem *    any purpose with or without fee is hereby granted, provided that the
+@rem *    above copyright notice and this permission notice appear in all
+@rem *    copies.
+@rem *
+@rem *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+@rem *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+@rem *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+@rem *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+@rem *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+@rem *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+@rem *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+@rem *     PERFORMANCE OF THIS SOFTWARE.
 @rem ******************************************************************************/
 @%SystemRoot%\syswow64\WindowsPowershell\v1.0\powershell -c (New-Object Media.SoundPlayer '%1').PlaySync();

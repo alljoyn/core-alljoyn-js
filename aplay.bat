37e0a8c469057128f853b31bd1f9cd8cc0e3fddd
C.J. Collier <cjcollier@linuxfoundation.org>
2016-12-01 19:05:36 -0800

    Replace AllSeenAlliance License with Apache License Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    
    Change-Id: I6a173a54d3ce3d5a85dd9c9abf99a46c21101b3b
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- aplay.bat
+++ aplay.bat
@@ -1,16 +1,12 @@
 @rem ******************************************************************************
-@rem * Copyright AllSeen Alliance. All rights reserved.
-@rem *
-@rem *    Permission to use, copy, modify, and/or distribute this software for any
-@rem *    purpose with or without fee is hereby granted, provided that the above
-@rem *    copyright notice and this permission notice appear in all copies.
-@rem *
-@rem *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-@rem *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-@rem *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-@rem *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-@rem *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-@rem *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-@rem *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+@rem * @rem * 
+@rem *    Copyright (c) 2016 Open Connectivity Foundation and AllJoyn Open
+@rem *    Source Project Contributors and others.
+@rem *    
+@rem *    All rights reserved. This program and the accompanying materials are
+@rem *    made available under the terms of the Apache License, Version 2.0
+@rem *    which accompanies this distribution, and is available at
+@rem *    http://www.apache.org/licenses/LICENSE-2.0
+
 @rem ******************************************************************************/
 @%SystemRoot%\syswow64\WindowsPowershell\v1.0\powershell -c (New-Object Media.SoundPlayer '%1').PlaySync();
